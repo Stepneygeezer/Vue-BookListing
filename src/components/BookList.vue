7b240e5b957ded1a9e67ec167d97cfdac2ae531b
Akeel Ahmed <akeelahmed@users.noreply.github.com>
2019-03-20 12:12:47 +0000

    Completed Module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,10 @@
 <template>
-    <div><h1>{{title}}</h1></div>
+    <div>
+        <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+         </ul> 
+    </div>
 </template>
 
 <script>
@@ -7,14 +12,27 @@ export default {
     name:'BookList',
     data(){
         return{
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+                {title:"Self-Reliance", author:"Neil 1"},
+                {title:"American Gods", author:"Neil 2"},
+                {title:"Amusing ourselves to death", author:"Neil Postman"},
+            ]
+            }
         }
     }
-}
+
 </script>
 
 <style>
 
 h1, h2 {font-weight: normal}
-
+ul{
+    list-style-type:none;
+    padding:0px;
+}
+li{
+    display:block;
+    margin: 0 10px;
+}
 </style>

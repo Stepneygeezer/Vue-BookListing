b7ae526cfa7559bbbf67478865a9d6cf35147711
Akeel Ahmed <akeelahmed@users.noreply.github.com>
2019-03-20 16:09:53 +0000

    Completed Module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li v-for="book in books">{{book.title}}:{{book.author}}</li>  
+</template>
+
+<script>
+export default {
+
+    name: 'Book',
+    props: ['book']
+}
+</script>
+
+<style>
+li{
+    display: block;
+    margin: 0 10px;
+}
+
+</style>
